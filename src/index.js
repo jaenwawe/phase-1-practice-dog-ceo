a239f96464c0d9d8d3f681416e92d789ebc949aa
jaenwawe <nwawe.jae@gmail.com>
2021-08-17 18:31:05 -0500

    adds photo from .json after promise

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
-console.log('%c HI', 'color: firebrick')
+const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
+
+fetch(imgUrl)
+    .then ((r) => r.json())  //get .json objects from website
+    .then((dogBreed) => {     // are we mapping the .json??
+    console.log(dogBreed)
+    console.log(dogBreed[0])
+    let dogImg = document.createElement("img") //create img on DOM
+    dogImg.src= dogBreed.message[0]; //add url to img src
+    console.log(dogImg) 
+    document.body.append(dogImg) //append the image to the body
+    })
+
+
